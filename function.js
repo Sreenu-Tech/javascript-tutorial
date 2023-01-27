e7491a00120057f68e7a6afb44275a147c220579
partharanjan <partharanjan.nayak@gmail.com>
2023-01-27 08:29:54 +0530

    function & object

--- function.js
+++ function.js
@@ -1,3 +1,37 @@
 /**
  * function
  */
+let num1 = 10;
+let num2 = 20;
+let sum = num1 + num2
+
+if (sum > 100) {
+    console.log('hi')
+}
+else {
+    console.log('bye')
+}
+
+let num3 = 20;
+let num4 = 50;
+let sum2 = num3 + num4
+if (sum2 > 100) {
+    console.log('hi')
+}
+else {
+    console.log('bye')
+}
+
+function dosum(n1, n2) {
+    let sum = n1 + n2
+    if (sum > 100) {
+        console.log('hi')
+    }
+    else {
+        console.log('bye')
+    }
+    return sum;
+}
+
+dosum(50,60)
+dosum(40,40);
---
+++ object.js
@@ -0,0 +1,6 @@
+let emp = {
+    name: 'Jav Script',
+    gender: 'Male',
+    dob: '1990-01-01',
+    salary: 12345
+}
